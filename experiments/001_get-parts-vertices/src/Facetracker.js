1a53104677283afc70924914255c3cf6211e6365
t-kiyotaki <t-kiyotaki@bfp.co.jp>
2016-06-30 10:41:18 +0900

    get-parts-vertices update

--- experiments/001_get-parts-vertices/src/Facetracker.js
+++ experiments/001_get-parts-vertices/src/Facetracker.js
@@ -5,6 +5,8 @@ export default class FaceTracker{
     
     constructor(){
         this.faceIndeces = new FaceIndeces();
+        this.faceParts = new FaceParts();
+
         this.vertices = [];
         this.tracker = new clm.tracker({useWebGL: true});
         this.model = defaultModel;
@@ -61,7 +63,17 @@ export default class FaceTracker{
         let cc = canvas.getContext("2d");
         cc.fillStyle = "rgb(192, 80, 77)"
 
-        this._drawPartsPoints(cc,this.edge);
+        this._drawPartsPoints(cc,this.faceParts.edge);
+        this._drawPartsPoints(cc,this.faceParts.upperLip);
+        this._drawPartsPoints(cc,this.faceParts.lowerLip);
+        this._drawPartsPoints(cc,this.faceParts.rightEyebrows);
+        this._drawPartsPoints(cc,this.faceParts.leftEyebrows);
+        this._drawPartsPoints(cc,this.faceParts.leftPupil);
+        this._drawPartsPoints(cc,this.faceParts.rightPupil);
+        this._drawPartsPoints(cc,this.faceParts.leftEye);
+        this._drawPartsPoints(cc,this.faceParts.rightEye);
+        this._drawPartsPoints(cc,this.faceParts.noseLine);
+        this._drawPartsPoints(cc,this.faceParts.noseEdge);
 
     }
 
@@ -73,15 +85,67 @@ export default class FaceTracker{
     }
 
     mapParts(){
-        this.edge = [];
+        this.faceParts.edge = [];
         this.faceIndeces.edge.map((idx)=>{
-            this.edge.push(this.positions[idx]);
+            this.faceParts.edge.push(this.positions[idx]);
+        });
+
+        this.faceParts.upperLip = [];
+        this.faceIndeces.upperLip.map((idx)=>{
+            this.faceParts.upperLip.push(this.positions[idx]);
         
         });
 
-        this.lowerLip = [];
+        this.faceParts.lowerLip = [];
         this.faceIndeces.lowerLip.map((idx)=>{
-            this.lowerLip.push(this.positions[idx]);
+            this.faceParts.lowerLip.push(this.positions[idx]);
+        
+        });
+
+        this.faceParts.leftEyebrows = [];
+        this.faceIndeces.leftEyebrows.map((idx)=>{
+            this.faceParts.leftEyebrows.push(this.positions[idx]);
+        
+        });
+
+        this.faceParts.rightEyebrows = [];
+        this.faceIndeces.rightEyebrows.map((idx)=>{
+            this.faceParts.rightEyebrows.push(this.positions[idx]);
+        
+        });
+
+        this.faceParts.leftPupil = [];
+        this.faceIndeces.leftPupil.map((idx)=>{
+            this.faceParts.leftPupil.push(this.positions[idx]);
+        
+        });
+
+        this.faceParts.rightPupil = [];
+        this.faceIndeces.rightPupil.map((idx)=>{
+            this.faceParts.rightPupil.push(this.positions[idx]);
+        
+        });
+
+        this.faceParts.leftEye = [];
+        this.faceIndeces.leftEye.map((idx)=>{
+            this.faceParts.leftEye.push(this.positions[idx]);
+        
+        });
+        this.faceParts.rightEye = [];
+        this.faceIndeces.rightEye.map((idx)=>{
+            this.faceParts.rightEye.push(this.positions[idx]);
+        
+        });
+
+        this.faceParts.noseLine = [];
+        this.faceIndeces.noseLine.map((idx)=>{
+            this.faceParts.noseLine.push(this.positions[idx]);
+        
+        });
+
+        this.faceParts.noseEdge = [];
+        this.faceIndeces.noseEdge.map((idx)=>{
+            this.faceParts.noseEdge.push(this.positions[idx]);
         
         });
     }
